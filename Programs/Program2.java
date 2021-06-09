cbfb78dbcd0de14c263ef0708d01404377454b8e
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-09 14:10:59 +0530

    new program added

---
+++ Programs/Program2.java
@@ -0,0 +1,9 @@
+public class Program2{
+  public static void main(String [] agrs)
+  {
+    System.out.println("comments");
+    // comment in line one.
+    /* Multi line comment */
+    /** Documentation comment  */
+  }
+}
