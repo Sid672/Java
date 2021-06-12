2c5a82e17e6109892d411429761571cad1d29c7d
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-12 17:58:58 +0530

    Delete Program2.java

--- Programs/Program2.java
+++
@@ -1,9 +0,0 @@
-public class Program2{
-  public static void main(String [] agrs)
-  {
-    System.out.println("comments");
-    // comment in line one.
-    /* Multi line comment */
-    /** Documentation comment  */
-  }
-}
