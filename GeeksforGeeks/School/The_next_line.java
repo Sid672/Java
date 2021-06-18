aea51b592ae9811eeb2437207f8f87e10810f423
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-18 16:55:27 +0530

    Create The_next_line.java

---
+++ GeeksforGeeks/School/The_next_line.java
@@ -0,0 +1,14 @@
+class Geeks{
+    
+    // Function to print each word in single line
+    static void printIndividualLine(){
+        
+        String s1 = "Geeks\n";
+        String s2 = "for\n";
+        String s3 = "Geeks\n";
+        
+        System.out.println(s1 + s2 + s3);
+        
+    }
+    
+}
