23a2778d033adb0e49b35e371719c1c16a3489a8
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-18 16:19:48 +0530

    Create java_basic_data_types.java

---
+++ GeeksforGeeks/School/java_basic_data_types.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        int a = sc.nextInt();
+        return a;
+    }
+    
+    String javaStringType(Scanner sc) {
+        String s = sc.next();
+        return s;
+    }
+    
+    float javaFloatType(Scanner sc) {
+        float f = sc.nextFloat();
+        return f;
+    }
