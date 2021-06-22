420f368feb6c252b99dbfb2158d591e376de0512
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-22 16:57:50 +0530

    Create Program16.java

---
+++ Programs/P11-20/Program16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class test{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+   
+        System.out.println("Enter number: ");
+		int a = sc.nextInt();
+
+
+		if (a % 2 == 0)
+		{
+			System.out.println(a + " is Even");
+		}
+		else
+		{
+			System.out.println(a + " is Odd");
+		}
+	}
+}
