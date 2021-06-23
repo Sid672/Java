33a975e44f9f895b79f48080b025bd37c4dbb9d0
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-23 09:55:13 +0530

    Create Program21.java

---
+++ Programs/P21_30/Program21.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class test{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter year: ");
+        int a = sc.nextInt();
+
+        if (a % 400 == 0)
+        {
+            System.out.println("Leap Year");
+        }
+
+        else if (a % 4 == 0 && a % 100 != 0)
+        {
+            System.out.println("Leap Year");
+        }
+
+        else
+        {
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
