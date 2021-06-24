a92cf4ca9aa2a000b113de12bf36790ef436a6d2
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-24 21:01:10 +0530

    Create Program28.java

---
+++ Programs/P21_30/Program28.java
@@ -0,0 +1,19 @@
+// Program to print N odd natural numbers:
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+     
+        System.out.println("Enter Number: ");
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++)
+		{
+			int odd = 2 * i - 1;
+			System.out.println(odd);
+		}
+	}
+}
