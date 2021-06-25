d1611cfaa91cf1124cefbc5fbfd95fddb1b06bdb
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-25 21:16:05 +0530

    Create Program32.java

---
+++ Programs/P31_40/Program32.java
@@ -0,0 +1,21 @@
+// Program to print sum of first n odd number.
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+	    int n = sc.nextInt();
+        int sum = 0;
+
+	    for (int i = 1; i <= n; i++)
+	    {
+	        sum += 2 * i - 1;
+	    }
+
+	    System.out.println("Sum = " + sum);
+	}
+}
