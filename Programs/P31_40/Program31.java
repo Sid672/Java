0c2ac4134b9d282963de7b936f2bcfaeac4e84fa
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-25 21:09:25 +0530

    Create Program31.java

---
+++ Programs/P31_40/Program31.java
@@ -0,0 +1,21 @@
+// Program to print sum of first n even number.
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String args[])
+	{
+		  Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter number: ");
+	    int n = sc.nextInt();
+      int sum = 0;
+
+	    for (int i = 1; i <= n; i++)
+	    {
+	        sum += 2 * i;
+	    }
+
+	    System.out.println("Sum = " + sum);
+	}
+}
