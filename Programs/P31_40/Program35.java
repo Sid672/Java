b4007826f9e106761c58d48828c4e0c3f75cb4cc
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-26 21:43:39 +0530

    Create Program35.java

---
+++ Programs/P31_40/Program35.java
@@ -0,0 +1,23 @@
+// Program to calcuate number of digits in a number.
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String args[])
+	{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        while (n > 0)
+        {
+        	n = n / 10;
+            cnt++;
+        }
+       
+        System.out.println("Digits = " + cnt);
+
+	}
+}
