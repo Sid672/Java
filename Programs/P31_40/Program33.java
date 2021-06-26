43fbcf0a4114f3385c7cb04191f8b12627531eaf
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-26 20:56:32 +0530

    Create Program33.java

---
+++ Programs/P31_40/Program33.java
@@ -0,0 +1,24 @@
+// Program to calcuate x to the power of y.
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String args[])
+	{
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter numbers: ");
+        int a = sc.nextInt();
+        int n = sc.nextInt();
+        int res = 1;
+
+   
+        for (int i = 0; i < n; i++)
+        {
+            res = res * a;
+        }
+
+        System.out.println("Ans = " + res);
+	}
+}
+
