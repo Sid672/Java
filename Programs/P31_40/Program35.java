2e52a2f4e147a354f5c776d991505768e6c9ef89
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-26 21:55:03 +0530

    Update Program35.java

--- Programs/P31_40/Program35.java
+++ Programs/P31_40/Program35.java
@@ -11,6 +11,12 @@ public class test{
         int n = sc.nextInt();
         int cnt = 0;
 
+        if (n == 0)
+        {
+	        System.out.println("Digits = " + 1);
+            return;
+        }
+
         while (n > 0)
         {
         	n = n / 10;
