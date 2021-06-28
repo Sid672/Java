242dc6e59aade44ce1c41a448a81212ee2697c38
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-28 15:17:02 +0530

    Create Program_40.java

---
+++ Programs/P31_40/Program_40.java
@@ -0,0 +1,24 @@
+//Program to calculate HCF
+
+import java.util.Scanner;
+
+public class test{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int min = a > b ? b : a; 
+        
+        for (int i = min; i > 0; i--)
+        {
+            if ((a % i == 0) && (b % i == 0))
+            {
+                System.out.println("HCF = " + i);
+                break;
+            }
+        }
+    }
+}
