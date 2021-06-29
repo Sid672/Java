cde75ee2db8269301fa377c11c0dba3476b324ce
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-29 14:26:16 +0530

    Create Program41.java

---
+++ Programs/P41_50/Program41.java
@@ -0,0 +1,30 @@
+//Program to check a number is prime or not.
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a number: ");
+		int n = sc.nextInt();
+
+		if (n == 1)
+		{
+			System.out.println("Neither prime nor composite");
+			return;
+		}
+
+		for (int i = 2; i < n; i++)
+		{
+			if (n % i == 0)
+			{
+				System.out.println("Composite");
+				return;
+			}
+		}
+
+		System.out.println("Prime");
+	}
+}
