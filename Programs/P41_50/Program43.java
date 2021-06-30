94ff3562c6c2beb8cab36ba0db23545dee7c0d4d
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-06-30 14:36:11 +0530

    Create Program43.java

---
+++ Programs/P41_50/Program43.java
@@ -0,0 +1,40 @@
+//Program to print prime number in a range.
+
+import java.util.Scanner;
+
+public class test{
+	public static void main(String []args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter a range: ");
+		int l = sc.nextInt();
+		int r = sc.nextInt();
+		int n;
+
+		if (l == 1)
+		{
+			l = 2;
+		}
+
+		for (int j = l; j <= r; j++)
+		{
+        boolean a = true;
+        n = j;
+        for (int i = 2; i * i <= n; i++)
+        {
+          if (n % i == 0)
+          {
+              a = false;
+              break;	
+          }
+        }
+
+        if (a)
+        {
+          System.out.print(n + " ");
+        }
+	   }
+     System.out.println();
+	}
+}
