36252f25d0a1a08419b3b3ecfa2a7ae600c763c8
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-07-05 13:08:36 +0530

    Update Program2.java

--- Text_book(SPPU)/Strings/Program2.java
+++ Text_book(SPPU)/Strings/Program2.java
@@ -5,10 +5,10 @@ Porgram to claculate length of string. (Page no. 3-41)
 public class test{
 	public static void main(String args[])
 	{
-		    String str[] = {'P', 'R', 'O', 'G', 'R', 'A', 'M'};
-        String s = new String(str);
+		char str[] = {'P', 'R', 'O', 'G', 'R', 'A', 'M'};
+		String s = new String(str);
 
-        System.out.println("String is " + s);
-        System.out.println("Length of string is " + s.length());
+		System.out.println("String is " + s);
+		System.out.println("Length of string is " + s.length());
 	}
 }
