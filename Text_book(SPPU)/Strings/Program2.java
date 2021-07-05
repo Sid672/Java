8f82c86ba3047d5a1307d08ba6f718104090e4ec
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-07-05 13:07:05 +0530

    Create Program2.java

---
+++ Text_book(SPPU)/Strings/Program2.java
@@ -0,0 +1,14 @@
+/*
+Porgram to claculate length of string. (Page no. 3-41)
+*/
+
+public class test{
+	public static void main(String args[])
+	{
+		    String str[] = {'P', 'R', 'O', 'G', 'R', 'A', 'M'};
+        String s = new String(str);
+
+        System.out.println("String is " + s);
+        System.out.println("Length of string is " + s.length());
+	}
+}
