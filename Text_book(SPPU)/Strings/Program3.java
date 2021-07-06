520495102949534e315d7c64db4f17979e2a33de
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-07-06 10:55:13 +0530

    Create Program3.java

---
+++ Text_book(SPPU)/Strings/Program3.java
@@ -0,0 +1,12 @@
+/*
+Program to concatenate strings. (Page no. 3-42)
+*/
+
+public class test{
+    public static void main(String args[])
+    {
+        String fruit = new String("mango");
+
+        System.out.println("I like " + fruit + " very much.");
+    }
+}
