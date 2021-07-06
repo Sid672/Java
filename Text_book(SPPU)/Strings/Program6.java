e62daa9a8d40673504532c89e5aacb138ed15ffb
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-07-06 11:06:33 +0530

    Create Program6.java

---
+++ Text_book(SPPU)/Strings/Program6.java
@@ -0,0 +1,13 @@
+/*
+Program to print sub-strings (Page 3-44)
+*/
+
+public class test{
+    public static void main(String args[])
+    {
+        String str = new String("I love my country very much");
+
+        System.out.println("One substring is :" + str.substring(2, 6));
+        System.out.println("Another substring is: " + str.substring(18));
+    }
+}
