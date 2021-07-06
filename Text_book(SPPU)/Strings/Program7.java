3425f0f8dbfe4216a1376edcb92a44d84aea1e56
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-07-06 11:10:04 +0530

    Create Program7.java

---
+++ Text_book(SPPU)/Strings/Program7.java
@@ -0,0 +1,15 @@
+/*
+Program to use replace char in string. (Page 3-44)
+*/
+
+public class test{
+    public static void main(String args[])
+    {
+        String str = new String("Nesha is Indian");
+
+        String s = str.replace('i', 'a');
+
+        System.out.println(s);
+        
+    }
+}
