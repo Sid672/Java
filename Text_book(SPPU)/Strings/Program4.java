8252f78346dd175a93e0a0fccfc9452bff115222
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-07-06 10:58:23 +0530

    Create Program4.java

---
+++ Text_book(SPPU)/Strings/Program4.java
@@ -0,0 +1,14 @@
+/*
+Program to concatenate strings (Page 3-42)
+*/
+
+public class test{
+    public static void main(String args[])
+    {
+        String fruit = new String("mango");
+
+        String str = new String("I like ");
+
+        System.out.println(str.concat(fruit));
+    }
+}
