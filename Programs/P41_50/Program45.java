ab73e42ac3fa93dc9727d6be36b8f66af9597899
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-08-16 17:46:40 +0530

    Create Program45.java

---
+++ Programs/P41_50/Program45.java
@@ -0,0 +1,24 @@
+//Program to draw pattern no. 26
+// A
+// BA
+// CBA
+// DCBA
+
+public class test{
+  public static void main(String args[])
+  {
+    char c = 'A';
+    char b = 'A';
+    for (int i = 0; i < 4; i++)
+    {
+      c = b;
+      for (int j = 0; j <= i; j++)
+      {
+        System.out.print(c);
+        c--;
+      }
+      System.out.println();
+      b++;
+    }
+  }
+}
