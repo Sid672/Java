36797c8fe96e6e4b2b3239e68800a88c9dc9da14
Sid672 <Siddharthsingh7898@gmail.com>
2021-08-24 19:01:41 +0530

    area of circle

---
+++ Programs/P51_60/Program55.java
@@ -0,0 +1,22 @@
+//This is a program to calculate
+//area of circle using function
+
+
+import java.util.Scanner;
+
+public class Program55{
+    static void Area(float r)
+    {
+        float pi = 3.14f;
+        float area = pi * r * r;
+        System.out.println(area);
+    }
+
+    public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter radius: ");
+         float r = sc.nextFloat();
+         Area(r);
+    }
+}
