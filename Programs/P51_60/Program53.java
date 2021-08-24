9ba5d4944dafaef5a081e68c35235ea4811ab5a6
Sid672 <Siddharthsingh7898@gmail.com>
2021-08-24 18:53:28 +0530

    factorial program

---
+++ Programs/P51_60/Program53.java
@@ -0,0 +1,28 @@
+//Program to calculate factorial
+//of a number using git
+
+import java.util.Scanner;
+
+public class Program53{
+
+	int fact(int n)
+	{
+	   int res = 1;
+       while (n > 1)
+       {
+       	  res = res * n;
+       	  n--;
+       }
+       return res;
+	}
+
+	public static void main(String args[])
+	{
+        Scanner sc = new Scanner(System.in);
+        Program53 obj = new Program53();
+        int  n = sc.nextInt();
+    
+        int res = obj.fact(n);
+        System.out.println("factorial of " + n + " : " + res);
+	}
+}
