21338d5f11a45365deae7afeb232cdaf795c3e76
Sid672 <Siddharthsingh7898@gmail.com>
2021-08-26 18:43:42 +0530

    TNRN

---
+++ Programs/P51_60/Program56.java
@@ -0,0 +1,24 @@
+// Program to print sum of n natural no. using function.
+// take nothing and return nothing.
+
+import java.util.Scanner;
+
+public class Program56{
+
+	static int n;
+
+	static void cal()
+	{
+		int sum = n * (n + 1) / 2;
+        System.out.println("Sum of " + sum);
+        return;
+	}
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a no.:");
+        n = sc.nextInt();
+        cal();
+	}
+}
