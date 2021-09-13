d7bd23b5479c4e343cd52214bbe3b4e9f007eb55
Sid672 <Siddharthsingh7898@gmail.com>
2021-09-13 18:08:39 +0530

    Use of array

---
+++ Programs/P51_60/Program59.java
@@ -0,0 +1,30 @@
+//Program with use of array
+
+import java.util.Scanner;
+
+public class Program59{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a[] = new int[5];
+        int sume = 0, sumo = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (a[i] % 2 == 0)
+            {
+                sume += a[i];
+            }
+            else
+            {
+               sumo += a[i];
+            }
+        }
+        System.out.println("Sum of odd number: " + sumo);
+        System.out.println("Sum of even number: " + sume);
+    }
+}
