352d26aaf885e96910a52a2657ea21dc85edbf88
Siddharth singh chouhan <63919605+Sid672@users.noreply.github.com>
2021-09-19 20:07:59 +0530

    extra newlines are not required.

--- Programs/P11-20/Program11.java
+++ Programs/P11-20/Program11.java
@@ -1,13 +1,10 @@
 /** program to print the integer value of string */
 public class Main {
-  public static void main(String[] agrs) {
-
+  public static void main(String[] agrs) 
+  {
       Integer i = Integer.valueOf("123");
-
       int a = i + 2;
-
       System.out.println(a);
-
       System.out.println(i);
   }
 }
